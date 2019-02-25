d8fa4332e14e12de245b57a577266b418cac5343
Prezlyata Yuriy <Prezlyata Yuriy>
2019-02-24 18:47:00 -0600

    set up routes & middlewares

---
+++ server.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+const app = express();
+
+app.set('view engine', 'hbs');
+hbs.registerPartials(__dirname + '/views/partials');
+
+app.use((req,res,next) => {
+    let now = new Date().toString();
+    let log = `${now}: ${req.method} ${req.url}`;
+    fs.appendFile('server.log', log + '\n', (err) => {
+        if (err) {
+            console.log('Unable to append to server.log.');
+        }
+    });
+    next();
+});
+app.use((req,res,next) => {
+    res.render('maintenance.hbs');
+});
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (req,res) => {
+    res.render(
+        'home.hbs',
+        {
+            pageTitle: 'Welcome to home page!',
+            currentYear: new Date().getFullYear()
+        }
+    );
+});
+app.get('/help', (req,res) => {
+    res.sendFile(__dirname + '/public/help.html');
+});
+app.get('/about', (req, res) => {
+    res.render(
+        'about.hbs',
+        {
+            pageTitle: 'Welcome to about page!',
+            currentYear: new Date().getFullYear()
+        }
+    );
+});
+
+app.listen(3000);
